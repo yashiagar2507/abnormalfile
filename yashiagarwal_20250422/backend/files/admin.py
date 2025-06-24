f34c81942205e5c3638607ca7519abb50851fa1b
yashiagar2507 <yagarwal2@horizon.csueastbay.edu>
2025-06-24 16:16:56 -0700

    Initial commit

--- yashiagarwal_20250422/backend/files/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import File  # ✅ your model
-
-@admin.register(File)
-class FileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'original_filename', 'file_type', 'size', 'uploaded_at')  # ✅ fields to show
-    search_fields = ('original_filename',)
